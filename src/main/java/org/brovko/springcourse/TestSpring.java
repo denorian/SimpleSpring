74a8ad6f443b951bcd7c72fb22a0d9549a4831c1
denorian <denorian@yandex.ru>
2019-12-27 11:58:36 +0300

    Spring Framework Lesson 5

--- src/main/java/org/brovko/springcourse/TestSpring.java
+++ src/main/java/org/brovko/springcourse/TestSpring.java
@@ -7,9 +7,8 @@ public class TestSpring {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml"
 		);
-		TestBean testBean = context.getBean("testBean",TestBean.class);
-		
-		System.out.println(testBean.getName());
+		MusicPlayer testBean = context.getBean("musicPlayer", MusicPlayer.class);
+		testBean.playMusic();
 		
 		context.close();
 	}
