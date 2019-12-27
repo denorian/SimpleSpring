73dd82522f807e9e3cb030be8f8b6591d89b0d26
denorian <denorian@yandex.ru>
2019-12-27 13:10:55 +0300

    Spring Framework Lesson 7

--- src/main/java/org/brovko/springcourse/TestSpring.java
+++ src/main/java/org/brovko/springcourse/TestSpring.java
@@ -7,8 +7,15 @@ public class TestSpring {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"applicationContext.xml"
 		);
-		MusicPlayer testBean = context.getBean("musicPlayer", MusicPlayer.class);
-		testBean.playMusic();
+		MusicPlayer testBean1 = context.getBean("musicPlayer", MusicPlayer.class);
+		MusicPlayer testBean2 = context.getBean("musicPlayer", MusicPlayer.class);
+		System.out.println(testBean1);
+		System.out.println(testBean2);
+		System.out.println(testBean1.getVolume());
+		System.out.println(testBean2.getVolume());
+		testBean1.setVolume(99);
+		System.out.println(testBean1.getVolume());
+		System.out.println(testBean2.getVolume());
 		context.close();
 	}
 }
